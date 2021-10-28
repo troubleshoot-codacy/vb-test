076760c7ae55213d1e1f461d800b70d55c3bf3d2
Dan Putman <dan@codacy.com>
2021-10-28 09:56:02 -0400

    Initial

---
+++ WorldWar2/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MainToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AddFactToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RemoveFactToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearListToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CloseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MainToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(407, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MainToolStripMenuItem
+        '
+        Me.MainToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AddFactToolStripMenuItem, Me.RemoveFactToolStripMenuItem, Me.ClearListToolStripMenuItem, Me.CloseToolStripMenuItem})
+        Me.MainToolStripMenuItem.Name = "MainToolStripMenuItem"
+        Me.MainToolStripMenuItem.Size = New System.Drawing.Size(46, 20)
+        Me.MainToolStripMenuItem.Text = "Main"
+        '
+        'AddFactToolStripMenuItem
+        '
+        Me.AddFactToolStripMenuItem.Name = "AddFactToolStripMenuItem"
+        Me.AddFactToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.AddFactToolStripMenuItem.Text = "Add Fact"
+        '
+        'RemoveFactToolStripMenuItem
+        '
+        Me.RemoveFactToolStripMenuItem.Name = "RemoveFactToolStripMenuItem"
+        Me.RemoveFactToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.RemoveFactToolStripMenuItem.Text = "Remove Fact"
+        '
+        'ClearListToolStripMenuItem
+        '
+        Me.ClearListToolStripMenuItem.Name = "ClearListToolStripMenuItem"
+        Me.ClearListToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ClearListToolStripMenuItem.Text = "Clear List"
+        '
+        'CloseToolStripMenuItem
+        '
+        Me.CloseToolStripMenuItem.Name = "CloseToolStripMenuItem"
+        Me.CloseToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.CloseToolStripMenuItem.Text = "Close"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 32)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(383, 173)
+        Me.ListBox1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(407, 217)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Facts About World War II"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MainToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AddFactToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RemoveFactToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearListToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CloseToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ WorldWar2/Form1.vb
@@ -0,0 +1,43 @@
+﻿'Ali Ahmad
+'CP Period 3
+'4/8/2019
+
+Public Class Form1
+    Dim fact As String
+    Private Sub AddFactToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AddFactToolStripMenuItem.Click
+        'This if statement asks for the yes or no for the messagebox
+        If MessageBox.Show("Would you like to enter a fact?", "World War 2 facts", MessageBoxButtons.YesNo) = Windows.Forms.DialogResult.Yes Then
+            'For loop for the times you eneter a fax, must be 5
+            For times As Integer = 0 To 5
+                If (times = 5) Then
+                    Exit For
+                    'This makes the input box for the fact and prints in list box. 
+                End If
+                fact = InputBox("Enter a fact", "World War 2 facts")
+                ListBox1.Items.Add(fact)
+            Next
+        End If
+    End Sub
+
+    Private Sub RemoveFactToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RemoveFactToolStripMenuItem.Click
+        'if statement asks if you want to remove a fact
+        If MessageBox.Show("Would you like to remove a fact?", "World War 2 facts", MessageBoxButtons.YesNo) = Windows.Forms.DialogResult.Yes Then
+            'make a new fact as well as remove previous fact. 
+            fact = InputBox("Enter a fact", "World War 2 facts")
+            ListBox1.Items.Remove(fact)
+        End If
+    End Sub
+
+    Private Sub ClearListToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearListToolStripMenuItem.Click
+        'This acts to clear the list
+        If MessageBox.Show("Would you like to clear the list?", "World War 2 facts", MessageBoxButtons.YesNo) = Windows.Forms.DialogResult.Yes Then
+            'this clears the list. 
+            ListBox1.Items.Clear()
+        End If
+    End Sub
+
+    Private Sub CloseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CloseToolStripMenuItem.Click
+        'closes form
+        Application.Exit()
+    End Sub
+End Class
